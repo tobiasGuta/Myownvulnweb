a6bd25f23685c0b4852ad413bd00aa7f9b6fc7de
tobiasGuta <156011774+tobiasGuta@users.noreply.github.com>
2025-03-16 17:36:54 -0400

    Create post1.php

---
+++ lfcvuln/post1.php
@@ -0,0 +1,3 @@
+<!-- post1.php -->
+<h2>Post 1: Welcome to My Website!</h2>
+<p>This is the first post. It's a simple introduction to our site. We hope you enjoy your stay and find useful content!</p>
