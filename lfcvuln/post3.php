2f1412c4b5c1f8212f472afabe355875545842d0
tobiasGuta <156011774+tobiasGuta@users.noreply.github.com>
2025-03-16 17:38:10 -0400

    Create post3.php

---
+++ lfcvuln/post3.php
@@ -0,0 +1,3 @@
+<!-- post3.php -->
+<h2>Post 3: Local File Inclusion Vulnerability</h2>
+<p>In this post, we'll learn about Local File Inclusion (LFI) and how it can be exploited. Read on to understand how attackers can misuse this vulnerability.</p>
