3e4f47983212870c358ebf402639b11669d0cfd6
tobiasGuta <156011774+tobiasGuta@users.noreply.github.com>
2025-03-16 17:37:49 -0400

    Create post2.php

---
+++ lfcvuln/post2.php
@@ -0,0 +1,3 @@
+<!-- post2.php -->
+<h2>Post 2: How to Hack PHP?</h2>
+<p>This post is about learning security in PHP, specifically exploring vulnerabilities like LFI and XSS. Stay tuned for more security tips!</p>
